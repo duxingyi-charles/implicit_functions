7c61135071dde26125a33876082d9d1f39a4acbd
CharlesDu <dohoney@126.com>
2023-12-11 11:01:45 -0800

    non-parametric general function

--- implicit_functions.h
+++ implicit_functions.h
@@ -15,6 +15,28 @@ public:
 };
 
 template <typename Scalar>
+class GeneralFunction : public ImplicitFunction<Scalar>
+{
+public:
+    GeneralFunction(
+        std::function<Scalar(Scalar, Scalar, Scalar, Scalar &, Scalar &, Scalar &)> func_grad) : func_grad_(func_grad) {}
+
+    Scalar evaluate(Scalar x, Scalar y, Scalar z) const override
+    {
+        Scalar gx, gy, gz;
+        return func_grad_(x, y, z, gx, gy, gz);
+    }
+
+    Scalar evaluate_gradient(Scalar x, Scalar y, Scalar z, Scalar &gx, Scalar &gy, Scalar &gz) const override
+    {
+        return func_grad_(x, y, z, gx, gy, gz);
+    }
+
+private:
+    std::function<Scalar(Scalar, Scalar, Scalar, Scalar &, Scalar &, Scalar &)> func_grad_;
+};
+
+template <typename Scalar>
 class ConstantFunction : public ImplicitFunction<Scalar>
 {
 public:
